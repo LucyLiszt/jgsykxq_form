b7c8f3735b35b4502882e83316d0e6fcbd471c30
haolei <43700466+LucyLiszt@users.noreply.github.com>
2019-03-24 09:48:38 +0800

    Create sykxq19.sql

---
+++ sykxq19.sql
@@ -0,0 +1,61 @@
+# Host: localhost  (Version: 5.5.53)
+# Date: 2019-03-24 08:51:23
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "19sykxq"
+#
+
+DROP TABLE IF EXISTS `19sykxq`;
+CREATE TABLE `19sykxq` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `radio` varchar(255) DEFAULT NULL,
+  `renshu` varchar(255) DEFAULT NULL,
+  `name1` varchar(255) DEFAULT NULL,
+  `studentID1` varchar(255) DEFAULT NULL,
+  `gender1` varchar(255) DEFAULT NULL,
+  `phone1` varchar(255) DEFAULT NULL,
+  `class1` varchar(255) DEFAULT NULL,
+  `major1` varchar(255) DEFAULT NULL,
+  `name2` varchar(255) DEFAULT NULL,
+  `studentID2` varchar(255) DEFAULT NULL,
+  `gender2` varchar(255) DEFAULT NULL,
+  `phone2` varchar(255) DEFAULT NULL,
+  `class2` varchar(255) DEFAULT NULL,
+  `major2` varchar(255) DEFAULT NULL,
+  `name3` varchar(255) DEFAULT NULL,
+  `studentID3` varchar(255) DEFAULT NULL,
+  `gender3` varchar(255) DEFAULT NULL,
+  `phone3` varchar(255) DEFAULT NULL,
+  `class3` varchar(255) DEFAULT NULL,
+  `major3` varchar(255) DEFAULT NULL,
+  `name4` varchar(255) DEFAULT NULL,
+  `studentID4` varchar(255) DEFAULT NULL,
+  `gender4` varchar(255) DEFAULT NULL,
+  `phone4` varchar(255) DEFAULT NULL,
+  `class4` varchar(255) DEFAULT NULL,
+  `major4` varchar(255) DEFAULT NULL,
+  `name5` varchar(255) DEFAULT NULL,
+  `studentID5` varchar(255) DEFAULT NULL,
+  `gender5` varchar(255) DEFAULT NULL,
+  `phone5` varchar(255) DEFAULT NULL,
+  `class5` varchar(255) DEFAULT NULL,
+  `major5` varchar(255) DEFAULT NULL,
+  `name6` varchar(255) DEFAULT NULL,
+  `studentID6` varchar(255) DEFAULT NULL,
+  `gender6` varchar(255) DEFAULT NULL,
+  `phone6` varchar(255) DEFAULT NULL,
+  `class6` varchar(255) DEFAULT NULL,
+  `major6` varchar(255) DEFAULT NULL,
+  `timu` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "19sykxq"
+#
+
+/*!40000 ALTER TABLE `19sykxq` DISABLE KEYS */;
+/*!40000 ALTER TABLE `19sykxq` ENABLE KEYS */;
